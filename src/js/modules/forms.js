72587935281dcc8a7d931461b88beb0f06166848
mainzonder <no7au53r@gmail.com>
2021-07-27 11:13:44 +0300

    added calculator

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,13 +1,14 @@
-const forms = () => {
-  const form = document.querySelectorAll("form"),
-    inputs = document.querySelectorAll("input"),
-    phoneInputs = document.querySelectorAll('input[name="user_phone"]');
+import checkNumInputs from './checkNumInputs'
 
-  phoneInputs.forEach(item => {
-    item.addEventListener('input', () => {
-      item.value = item.value.replace(/\D/, "");
-    });
-  });
+
+const forms = (state) => {
+  
+  const form = document.querySelectorAll("form"),
+    inputs = document.querySelectorAll("input");
+
+  checkNumInputs('input[name="user_phone"]');
+
+
 
   const message = {
     loading: "Загрузка ...",
@@ -31,11 +32,20 @@ const forms = () => {
   form.forEach((item) => {
     item.addEventListener("submit", (e) => {
       e.preventDefault();
+
       let statusMessage = document.createElement("div");
       statusMessage.classList.add("status");
       item.appendChild(statusMessage);
 
       const formData = new FormData(item);
+   
+        for (let key in state) {
+          formData.append(key, state[key])
+        }
+      // if (item.getAttribute('data-calc') === "end" ) {
+    
+      //  }
+   
 
       postData("assets/server.php", formData)
         .then((res) => {
@@ -48,9 +58,11 @@ const forms = () => {
           setTimeout(() => {
             statusMessage.remove();
           }, 5000);
-        });
-    });
-  });
-};
+        })
+    },
+    )
+  }
+  )
+}
 
 export default forms;
